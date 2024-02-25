563c41defab98e91912686349f7645e449b40504
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-26 00:35:30 +0500

    salom

--- src/components/Card/HeadCard/index.jsx
+++ src/components/Card/HeadCard/index.jsx
@@ -1,12 +1,13 @@
 import "./style.css"
 import "../../../css/GlobalCss/style.css"
 import SocialCard from "../socialcard";
+import img from "../../../Assets/me.JPG"
     
 
 function Globecard() {
     return (
         <div className="head">
-            <div className="relative">
+            <div className="relative grid md:grid-cols-2 grid-cols-1">
                 <div className="and">
                     <SocialCard />
                 </div>
@@ -19,7 +20,7 @@ function Globecard() {
                         </div>
                     </div>
                 </a>
-                <div className="headcard absolute"></div>
+                <div className="headcard absolute overflow-hidden"><img src={img} alt="" /></div>
                 <div className="absolute-card1 "></div>
                 <div className="absolute-card2 "></div>
                 <div className="absolute-card3 headshadow"></div>
