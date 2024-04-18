9e2ffe887683d176a657e42ae014c121758e135c
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-04-18 14:10:08 +0500

    projectts tailwind css ga ozgartirildi

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,35 +33,35 @@ function Projects() {
   const scrollTab = useRef();
   CustomHook(scrollTab, divs);
   return (
-    <section className='projects z-50' ref={scrollTab}>
-      <div className="title" ref={(el) => el && divs.current.push(el)}>
+    <section className='z-50' ref={scrollTab}>
+      <div className="font-museo-moderno font-system mt-0 text-center text-[5vw]" ref={(el) => el && divs.current.push(el)}>
         My Projects
       </div>
-      <div className="des" ref={(el) => el && divs.current.push(el)}>
+      <div className=" text-center max-w-[500px] m-auto" ref={(el) => el && divs.current.push(el)}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis quae veniam amet nesciunt voluptatibus quis consectetur consequatur quisquam harum.
       </div>
-      <div className="list">
+      <div className="list mt-14">
         {
           listProjects.map((value, key) => (
-            <div className='item' key={key} ref={(el) => el && divs.current.push(el)}>
-              <div className="images">
-                <img src={value.images} alt="" />
+            <div className='item  grid grid-cols-2 justify-between items-center mb-24 gap-20' key={key} ref={(el) => el && divs.current.push(el)}>
+              <div className="col-start-2 col-end-3 bg-gradient-to-tr from-[#1808ae87] to-[#3600fb68] p-10 text-center rounded-xl overflow-hidden">
+                <img className='h-[400px] rounded-xl shadow-md' src={value.images} alt="projects rasmlari" />
               </div>
-              <div className="content">
-                <h3>{value.name}</h3>
-                <div className="des">{value.des}</div>
-                <div className="mission">
-                  <div><FontAwesomeIcon icon={faPersonCircleQuestion} /></div>
+              <div className="col-start-1 col-end-2 row-start-1">
+                <h3 className='text-5xl m-0'>{value.name}</h3>
+                <div className="text-left w-full text-white mt-5">{value.des}</div>
+                <div className=" grid grid-cols-[70px,1fr] items-center gap-3 mt-3">
+                  <div className='bg-[#cccccc54] w-14 h-14 text-xl flex justify-center items-center rounded-md'><FontAwesomeIcon icon={faPersonCircleQuestion} /></div>
                   <div>
                     <h4>Mission</h4>
-                    <div className="des">{value.mission}</div>
+                    <div className="">{value.mission}</div>
                   </div>
                 </div>
-                <div className="mission">
-                  <div><FontAwesomeIcon icon={faEarthAmericas} /></div>
+                <div className="grid grid-cols-[70px,1fr] items-center gap-3 mt-3">
+                  <div className='bg-[#cccccc54] w-14 h-14 text-xl flex justify-center items-center rounded-md'><FontAwesomeIcon icon={faEarthAmericas} /></div>
                   <div>
                     <h4>Languages</h4>
-                    <div className="des">{value.language}</div>
+                    <div className="">{value.language}</div>
                   </div>
                 </div>
               </div>
