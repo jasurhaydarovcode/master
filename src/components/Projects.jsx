1098e3fb330d7fd0f96838ff9c74b683398388c0
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-04-27 19:56:57 +0500

    button qosildi

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import CustomHook from './CustomHook';
 import Project1 from "../Assets/projects/Reactideas.png"
 import LinkBtn from './buttons/link';
 import Tegs from './tegs';
+import Btnfirst from './buttons/First';
 function Projects() {
   const [listProjects] = useState([
     {
@@ -30,17 +31,15 @@ function Projects() {
     },
 
   ]);
-  const divs = useRef([]);
-  const scrollTab = useRef();
-  CustomHook(scrollTab, divs);
-  
+
+
   return (
-    <section ref={scrollTab}>
-      <div className="font-museo-moderno font-system mt-0 mb-10 text-center text-[5vw] flex justify-center items-end" ref={(el) => el && divs.current.push(el)}>
+    <section>
+      <div className="font-museo-moderno font-system mt-0 mb-10 text-center text-[5vw] flex justify-center items-end" >
         <Tegs teg={"h1"} className=""><p>My Projects</p></Tegs>
       </div>
 
-      <div className=" text-center max-w-[500px] m-auto flex justify-center items-end" ref={(el) => el && divs.current.push(el)}>
+      <div className=" text-center max-w-[500px] m-auto flex justify-center items-end" >
         <Tegs teg={"p"}>
           <p className='px-3'>Lorem ipsum dolor sit amet.</p>
         </Tegs>
@@ -48,34 +47,34 @@ function Projects() {
       <div className="list mt-14">
         {
           listProjects.map((value, key) => (
-            <div className='h-max grid grid-cols-2 justify-between items-center mb-56 gap-20' key={key} ref={(el) => el && divs.current.push(el)}>
-              <div className="col-start-2 col-end-3 bg-gradient-to-tr from-[#1808ae87] to-[#3600fb68] p-10 text-center rounded-xl overflow-hidden">
+            <div className='h-max grid grid-cols-2 justify-between items-center mb-56 gap-20' key={key} >
+              <div className="rel col-start-2 col-end-3 bg-gradient-to-tr from-[#100e2887] to-[#3600fb68] p-10 text-center rounded-xl overflow-hidden">
                 <img className='h-[400px] rounded-xl shadow-md' src={value.images} alt="projects rasmlari" />
               </div>
               <div className="col-start-1 col-end-2 row-start-1">
                 <Tegs teg={"h2"} row={true}>
                   <h3 className='text-5xl m-0'>{value.name}</h3>
-                </Tegs>
-                <Tegs teg={"p"} row={true}>
                   <p className="text-left w-full text-white mt-5">{value.des}</p>
                 </Tegs>
-                <div className=" grid grid-cols-[70px,1fr] items-center gap-3 mt-3">
+
+                <div className="rel grid grid-cols-[70px,1fr] items-center gap-3 mt-10">
                   <div className='bg-[#cccccc54] w-14 h-14 text-xl flex justify-center items-center rounded-md'><FontAwesomeIcon icon={faPersonCircleQuestion} /></div>
                   <div>
                     <h4>Mission</h4>
                     <div className="">{value.mission}</div>
                   </div>
                 </div>
-                <div className="grid grid-cols-[70px,1fr] items-center gap-3 mt-3">
+                <div className="rel grid grid-cols-[70px,1fr] items-center gap-3 mt-3">
                   <div className='bg-[#cccccc54] w-14 h-14 text-xl flex justify-center items-center rounded-md'><FontAwesomeIcon icon={faEarthAmericas} /></div>
                   <div>
                     <h4>Languages</h4>
                     <div className="">{value.language}</div>
                   </div>
                 </div>
-                <div className="grid grid-cols-[70px,1fr] items-center gap-3 mt-3">
-                  <div className='relative z-40'>
-                    <LinkBtn />
+                <div className="grid grid-cols-[70px,1fr] items-center gap-3 mt-10">
+                  
+                  <div >
+                    <Btnfirst text='github'/>
                   </div>
                 </div>
               </div>
--- src/components/buttons/First/index.jsx
+++ src/components/buttons/First/index.jsx
@@ -1,9 +1,16 @@
 import "./style.css"
 
-function Btnfirst() {
+function Btnfirst({ text = "btn", }) {
     return (
         <>
-            <button class='glowing-btn absollute bottom-0 z-10'><span class='glowing-txt'>C<span class='faulty-letter'>L</span>ICK</span></button>
+            <button class="btn rel">
+                <span class="text">Text</span>
+                <span class="flip-front">Front</span>
+                <span class="flip-back"><svg fill="#000000" height="50px" width="50px" viewBox="0 0 206.089 206.089" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M142.236,166.897l-10.607-10.606l53.247-53.247l-53.247-53.247l10.607-10.606l63.853,63.853L142.236,166.897z
+    M163.663,103.044L123.32,62.702H0v80.686h123.32L163.663,103.044z"/>
+                </svg></span>
+            </button>
         </>
     );
 }
