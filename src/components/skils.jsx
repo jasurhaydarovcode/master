88bfc5e10cb362759e16c51afba8df2afd028183
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-05-01 13:55:29 +0500

    blyaaaaa

--- src/components/skils.jsx
+++ src/components/skils.jsx
@@ -22,7 +22,7 @@ function Skils() {
     return (
         <section className=" mx-auto flex justify-center items-center min-h-screen h-max bg-transparent generall__border mb-10">
             <div className="absolute w-full h-[700px] flex justify-center items-center gap-2 flex-wrap overflow-hidden ">
-                <div class="skills generall__border">
+                <div class=" generall__border">
                     <div className="text-center text-white flex justify-center items-end">
                         <Tegs teg={"h1"}>
                             <h1 className=" text-8xl mb-5 mt-0-lg"> My Skills </h1>
@@ -30,18 +30,7 @@ function Skils() {
                     </div>
                     <div className="w-full p-20">
                         <div className="flex justify-between items-center w-full flex-wrap">
-                            {/* {data.map((item, index) => (
-                                <div className=" skills__card mb-2 generall__border" id="skills" key={index}>
-                                    <div className="">
-                                        <img src={item.image} alt="react js logo" />
-                                    </div>
-                                    <p className="p-incard text-center rounded-3xl">{item.text}</p>
-                                </div>
-                            ))} */}
                             <Card />
-                            <div className="App">
-
-                            </div>
                         </div>
                     </div>
                 </div>
